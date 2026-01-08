d2c13da1a1bcc0a4c7537b0a7f0e8b2cab817d4d
Anders Lynge Ravnsbæk <90785699+lynge96@users.noreply.github.com>
2026-01-08 17:43:42 +0100

    obasis opsætning af tests-projekt

---
+++ tests/Reveries.Application.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Reveries.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/Reveries.Core.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Reveries.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
